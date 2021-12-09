ec893f18ddfc8399698599fc436508b345c405cc
Sajjan <iamsajjanshahwork@gmail.com>
2021-12-09 18:46:23 +0530

    3 different component combined and only one useEffect and one base API URL used

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,17 @@
 import React from "react";
 import "./weather.css";
 import axios from "axios";
-import WeatherThirty from "./WeatherThirty";
-import FiveDay from "./FiveDay";
-import WeatherSixteen from "./WeatherSixteen";
+// import FiveDay from "./FiveDay";
 
-
-const currentWeather ="https://community-open-weather-map.p.rapidapi.com/weather";
-
-
+const API_BASE_URL = "https://community-open-weather-map.p.rapidapi.com";
 
 const Weather = () => {
-
   const [data, setData] = React.useState("");
   const [answer, SetAnswer] = React.useState("");
   const [check, setCheck] = React.useState(false);
-
+  const [weatherthirty, setWeatherThirthy] = React.useState("");
+  const [weather, setWeather] = React.useState("");
+  const [five, setFive] = React.useState();
 
   function clickHandler() {
     setCheck((prev) => {
@@ -31,7 +27,69 @@ const Weather = () => {
 
   React.useEffect(() => {
     axios
-      .get(currentWeather, {
+      .get(API_BASE_URL + "/weather", {
+        params: {
+          q: answer || "noida",
+        },
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key":
+            "011f22e004msh454fb3bbf894d8fp19efeajsneb48311842f1",
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(
+        API_BASE_URL + "/climate/month",
+        {
+          params: {
+            q: answer || "noida",
+          },
+          headers: {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key":
+              "011f22e004msh454fb3bbf894d8fp19efeajsneb48311842f1",
+          },
+        },
+        {}
+      )
+      .then((response) => {
+        setWeatherThirthy(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    axios
+      .get(
+        API_BASE_URL + "/forecast/daily",
+        {
+          params: {
+            q: answer || "noida",
+          },
+          headers: {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key":
+              "2322907e88msh323aeb45d46445bp1e87e5jsn445088b15555",
+          },
+        },
+        {}
+      )
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    axios
+      .get(API_BASE_URL + "/forecast", {
         params: {
           q: answer || "noida",
         },
@@ -42,17 +100,42 @@ const Weather = () => {
         },
       })
       .then((response) => {
-        setData(response.data);
+        setFive(response.data);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error.message);
       });
   }, [answer]);
 
+  const weatherList1 = weatherthirty?.list?.map((el) => (
+    <>
+      <p key={el.uniqueId}>{"Humidity --> " + el.humidity + "kg-1"}</p>
+      <p key={el.uniqueId}>{"Pressure -->" + el.pressure + "N/m2"}</p>
+      <p key={el.uniqueId}>{"WindSpeed -->" + el.wind_speed + "m/s"}</p>
+    </>
+  ));
+
+  const weatherList = weather?.list?.map((el, uid) => (
+    <div>
+      <p key={el.uniqueId}>{"Sunrise --> " + el.sunrise}</p>
+      <p key={el.uniqueId}>{"Sunset -->" + el.sunset}</p>
+      <p key={el.uniqueId}>{"Gust -->" + el.gust}</p>
+    </div>
+  ));
+  const fivedayElement = five?.list?.map((el, uid) => (
+    <div>
+      <p key={el.uniqueId}>{"humidity -->" + el.main.humidity}</p>
+      <p key={el.uniqueId}>{"Temperature -->" + el.main.temp}</p>
+      <p key={el.uniqueId}>{"Feels Like -->" + el.main.feels_like}</p>
+      <p key={el.uid}>{"Minimum Temperature -->" + el.main.temp_min}</p>
+      <p key={el.uniqueId}>{"Maximum Temperature -->" + el.main.temp_max}</p>
+      <p key={el.uniqueId}>{"Pressure-->" + el.main.pressure}</p>
+    </div>
+  ));
 
   return (
     <>
-      <h2 class="main">Real Time Weather Application</h2>
+      <h2 className="main">Real Time Weather Application</h2>
       <div className="search">
         <input
           type="text"
@@ -64,8 +147,9 @@ const Weather = () => {
       <div className="container">
         {data?.weather?.map((el) => (
           <div>
-            <h1> {el.description} </h1>
+            <h1 key={el.uniqueId}> {el.description} </h1>
             <img
+              key={el.uniqueId}
               src={"http://api.openweathermap.org/img/w/" + el.icon + ".png"}
               alt="please check internet"
             />
@@ -73,9 +157,18 @@ const Weather = () => {
         ))}
       </div>
       <div className="flex-container">
-        <WeatherThirty answer = {answer} clickHandler = {clickHandler} check = {check} />
-        <WeatherSixteen answer = {answer} clickHandler = {clickHandler} check  = {check} />
-        <FiveDay answer={answer} clickHandler = {clickHandler} check = {check}  />
+        <button onClick={clickHandler} className="thirty">
+          30 days{" "}
+        </button>
+        <div className="thirty-container">{check && weatherList1}</div>
+        <button className="sixteen" onClick={clickHandler}>
+          16 Days
+        </button>
+        <div className="sixteen-container">{check && weatherList}</div>
+        <button className="fiveday-btn" onClick={clickHandler}>
+          5 days
+        </button>
+        <div className="fiveday-container">{check && fivedayElement}</div>
       </div>
     </>
   );
